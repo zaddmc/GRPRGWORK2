51cd9f8cd984dea70c1a45ad7affc132ca28c39c
zaddmc <davidbk927@gmail.com>
2024-11-23 21:10:47 +0100

    user can select which thermostat to use

--- fridge.py
+++ fridge.py
@@ -1,16 +1,41 @@
+import numpy as np
+
 class fridge:
-    def __init__(self, start_temp:float = 5, room_temp:float = 20, compressor_temp:float = -5):
+    def __init__(self, start_temp:float = 5, room_temp:float = 20, compressor_temp:float = -5, electric_price = []):
         self.tempeture:float = start_temp
         self.room_tempeture:float = room_temp
         self.compressor_tempeture:float = compressor_temp
         self.tempeture_change = []
         self.door_val = {False : 5 * 10**-7, True : 3 * 10**-5} # Same as c1
         self.comp_val = {False : 0, True : 8 * 10**-6} # Same as c2
+        self.electricity_price = electric_price
+        self.cost_change = []
+        self.cost:float = 0
         return
 
-    def update_tempeture(self, is_door_open:bool, is_compressor_on:bool):
+    def update_tempeture(self, n:int , is_door_open:bool, is_compressor_on:bool):
         new_temp = self.tempeture + (self.door_val[is_door_open]*(self.room_tempeture - self.tempeture) + self.comp_val[is_compressor_on]*(self.compressor_tempeture - self.tempeture)) * 300
         self.tempeture = new_temp
         self.tempeture_change.append(new_temp)
+        
+        self.calculate_cost(n, is_compressor_on)
         return
-    
+
+    def calculate_cost(self, n:int, compressor_state):
+        add_cost:float = 0
+        if compressor_state:
+            add_cost += self.electricity_price[n]
+        add_cost += self.food_cost()
+
+        self.cost_change.append(add_cost)
+        self.cost += add_cost
+        return
+
+    def food_cost(self):
+        T = self.tempeture
+        if T < 3.5:
+            return 4.39 * np.exp(-0.49 * T)
+        elif 3.5 <= T and T < 6.5:
+            return 0
+        else:
+            return 0.11 * np.exp(0.31 * T)
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fridge import fridge
+
 import csv
 from datetime import datetime
 import random
+import numpy as np
+
+import matplotlib.pyplot as plt
 
 class your_mom:
     def __init__(self):
@@ -21,20 +25,43 @@ class your_mom:
         return
 
     def start(self):
-        
+        # Setup
         self.import_price()
         
-        for _ in range(20):
-            print(self.simple()) 
+         
+        # Get the thermostat type from user
+        thermostat_caller = 0
+        print("Select which thermostat yoo want\ns | simple : For the simple thermostat")
+        match input().lower():
+            case "simple" | "s" | _:
+                thermostat_caller = self.simple
+        
+        # Run the user selected thermostat
+        avg_cost = 0
+        runs = 20
+        for i in range(runs):
+            avg_cost += thermostat_caller()
+        avg_cost = avg_cost / runs 
+        
+
+        print(avg_cost)
+
+        #self.plot_price()
         return
     
     def simple(self):
-        my_fridge = fridge()
-        for _ in range(8640):
-            enable_comprssor = my_fridge.tempeture > 5
-            my_fridge.update_tempeture(random.randrange(9) == 0, enable_comprssor)
-        return my_fridge.tempeture       
+        my_fridge = fridge(electric_price=self.price)
+        for i in range(8640):
+            enable_comprssor = my_fridge.tempeture > 5 # The simple thermostat logic
+            is_door_open = random.randrange(9) == 0 # 1 in 10 chance of door being open
+            my_fridge.update_tempeture(i, is_door_open, enable_comprssor)
+        return my_fridge.cost      
 
+    def plot_price(self):
+        x = self.dates
+        y = self.price
+        plt.plot(x,y)
+        plt.show()
 
 if __name__ == "__main__":
     starter = your_mom()
